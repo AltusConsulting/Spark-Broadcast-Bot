f9d17373667b98db585170c3c19c406cbf94ae7e
Rafael Campos <rcampos@altus.cr>
2017-10-20 11:23:03 -0600

    Allowed all REST methods to avoid CORS problems

--- bot.js
+++ bot.js
@@ -97,6 +97,7 @@ controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
 
     webserver.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
